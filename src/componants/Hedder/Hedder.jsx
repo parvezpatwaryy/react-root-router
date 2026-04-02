b8b93c9e7cab0a2c99db0614b1a04ef462ea54d9
parvezpatwaryy <tanjilapatwaryy@gmail.com>
2026-04-03 00:21:31 +0600

    second commit

--- src/componants/Hedder/Hedder.jsx
+++ src/componants/Hedder/Hedder.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
+import './hedder.css';
 
 const Hedder = () => {
   return (
     <div>
       <h3>This is hedder</h3>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movielse">Movielse</Link>
+        <Link to="/laptops">Laptops</Link>
+        <Link to="/">Home</Link>
+      </nav>
     </div>
   );
 };
